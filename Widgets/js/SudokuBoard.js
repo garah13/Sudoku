eaab3b7ef730bfe5e26fee1f2d8377f4f157ff1f
Sarah Gilmore <sarahgilmore@Sarahs-MacBook-Pro-2.local>
2017-06-17 09:42:37 -0400

    first files

---
+++ Widgets/js/SudokuBoard.js
@@ -0,0 +1,43 @@
+/**
+ * Created by sarahgilmore on 6/14/17.
+ */
+define([
+    "dojo/_base/declare",
+	"dojo/query",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetsInTemplateMixin",
+    "SudokuCell",
+    "dojo/text!../templates/SudokuBoard.html"
+], function(declare, query, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, SudokuCell, template) {
+
+    return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+        widgetsInTemplate: true,
+        templateString: template,
+
+        constructor: function(args) {
+            //TODO: args should have the initial state of hte board in an array when instantiating
+
+            this.sudokuCellArrays = [];
+            this.numberCells = 4;
+            for (var i = 1; i <= this.numberCells; i++) {
+                var cell = new SudokuCell({'cellNumber': i});
+                this.sudokuCellArrays.push(cell);
+            }
+        },
+
+        postCreate: function() {
+            var widget = this;
+            console.log(this.sudokuCellArrays.length);
+            for (var i = 0; i < this.sudokuCellArrays.length; i++) {
+                var cell = this.sudokuCellArrays[i];
+                cell.placeAt(widget.sudokuBoardContainer, "last");
+            }
+        },
+
+	    _addEventListener: function() {
+
+	    }
+
+    });
+});
---
+++ Widgets/js/SudokuCell.js
@@ -0,0 +1,68 @@
+define([
+    "dojo/_base/declare",
+    "dojo/on",
+    "dojo/mouse",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dojo/text!../templates/SudokuCell.html"
+  ], function(declare, on, mouse, domClass, domStyle, _WidgetBase, _TemplatedMixin, template) {
+
+    return declare([_WidgetBase, _TemplatedMixin], {
+        templateString: template,
+
+        constructor: function(args) {
+            this.cellNumber = args.cellNumber;
+            this.cellValue = args.cellValue;
+            this.canSetCellValue = true;
+            if (!this.cellValue) {
+                this.canSetCellValue = false;
+                //add a class saying unmodifiable
+	            domClass.add(this.domNode, "disabledSudokuCell");
+            }
+
+            //Display number in svg
+        },
+
+        postCreate: function() {
+            if (this.canSetCellValue) {
+	            this._addEventListeners();
+            }
+        },
+
+        _addEventListeners: function() {
+            var widget = this;
+
+            on(this.sudokuCell, mouse.enter, function() {
+               domStyle.set(widget.sudokuRectangle, "fill", "red");
+            });
+
+	        on(this.sudokuCell, mouse.leave, function() {
+		        domStyle.set(widget.sudokuRectangle, "fill", "black");
+	        });
+        },
+
+        getCellValue: function() {
+            return this.cellValue;
+        },
+
+        getCellNumber: function() {
+            return this.cellNumber;
+        },
+
+        setCellValue: function(value) {
+            if (this.canSetCellValue) {
+                if (0 < value && value < 10) {
+                    this.cellValue = value;
+
+                }
+            }
+        },
+
+        isCellModifiable: function() {
+            return this.canSetCellValue;
+        }
+
+    });
+});
